e8691bfbdcb9654d7dcc7d886c86d579088f8018
shivam kumar <kumarshi605@gmail.com>
2023-02-21 20:02:41 +0530

    Created Home Activity

---
+++ app/src/main/java/com/example/adminavhallbooking/ui/timeslots/TimeSlotFragment.java
@@ -0,0 +1,38 @@
+package com.example.adminavhallbooking.ui.timeslots;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.adminavhallbooking.R;
+
+public class TimeSlotFragment extends Fragment {
+
+    private TimeSlotViewModel mViewModel;
+
+    public static TimeSlotFragment newInstance() {
+        return new TimeSlotFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_time_slot, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(TimeSlotViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
